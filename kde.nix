1bebafcf4a45be6a6d86086fb96c106af912081e
Krabaratos <63770336+krabaratos@users.noreply.github.com>
2026-06-26 11:13:05 +0530

    Add files via upload

---
+++ kde.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable the KDE Plasma Desktop Environment.
+  
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "sddm-astronaut-theme";
+    wayland.enable = true;
+    extraPackages = with pkgs; [ 
+      kdePackages.qtmultimedia
+      kdePackages.qtsvg
+      kdePackages.qtvirtualkeyboard
+      kdePackages.qtbase ];
+  };
+  services.desktopManager.plasma6.enable = true;
+
+  # Optional: add KDE apps
+  environment.systemPackages = with pkgs; [
+  darkly
+  klassy
+  kdePackages.kate
+  kdePackages.kcalc
+  kdePackages.kdialog
+  kdePackages.flatpak-kcm
+  kdePackages.kpipewire
+  kdePackages.sddm-kcm
+  libportal-qt6
+  sddm-astronaut
+  ];
+}
+
+
+
+
